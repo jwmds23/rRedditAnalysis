4c97106cb6600d03d1e345ea4134341c8999fc85
Nan Tang <dejaytang@gmail.com>
2024-01-27 17:28:43 -0800

    added R file for function get_threads_summary

--- get_threads_summary.R
+++ get_threads_summary.R
@@ -1,20 +1,7 @@
----
-title: "Nan Tang's Journal"
-author: "Nan Tang"
-date: "2024-01-27"
-output: html_document
----
-## Jan 25, 2024
-Worked on project proposal, created function structure, sample data and visualization charts.
-
-## Jan 26, 2024
-Created personal github branch and started a journal to record everyday updates on this R package.
-
-## Jan 27, 2024
-Start to connect reddit API according to this code:
-```{r}
 library(httr)
 library(jsonlite)
+
+## configure parameters for connecting the Reddit API
 client_id <- "JL7d1_eeiz7zeNLDwgEF5A"
 client_secret <- "XUJEFa86M5-wWMXkhHRNFOhMODv0UQ"
 user_agent <- "R:reddit_script:v1.0 (by /u/appleontree1990)"
@@ -28,23 +15,22 @@ response <- POST("https://www.reddit.com/api/v1/access_token",
 stop_for_status(response)
 content <- httr::content(response)
 access_token <- content$access_token
-```
 
-And extract data according to this code:
-```{r}
+get_threads_summary <- function(keyword){
+  
 # Define the API endpoint URL
 params <- list(
-sort = 'relevance',
-t = 'all',
-type ='link',
-q = keyword, # any keyword
-limit = 100,
-Authorization = access_token)
+  sort = 'relevance',
+  t = 'all',
+  type ='link',
+  q = keyword, # any keyword
+  limit = 100,
+  Authorization = access_token)
 
 # Send the GET request
 url <- paste0("https://www.reddit.com/search.json?q=",params$q)
 response <- GET(url, query = params)
 # Check the status of the response
 print(status_code(response))
-```
 
+}
